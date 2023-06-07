1318f1f8178dceb78eb89099df612048824c1b36
Istvan Feher <62614194+128478203-myseneca@users.noreply.github.com>
2023-06-07 10:43:27 -0400

    new try and except

--- function.py
+++ function.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 
 def calculate_age():
-    current_year = datetime.now().year
-    birth_year = int(input("Enter your birth year: "))
-    
-    age = current_year - birth_year
-    return age
+    try:
+        current_year = datetime.now().year
+        birth_year = int(input("Enter your birth year: "))
+        
+        age = current_year - birth_year
+        return age
+    except TypeError:
+        print("Please enter an integer for the birth year.")
 
 age = calculate_age()
-print("Your age is:", age)
+if age is not None:
+    print("Your age is:", age)
 
 def helloWorld():
     print(‘Hello World’)
